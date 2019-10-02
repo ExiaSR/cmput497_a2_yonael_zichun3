909725a43e74dcccaad0ccf14ad5185f95270625
Michael Lin <michael.lin1@ibm.com>
2019-10-02 14:49:33 -0600

    unsmoothed model is done

--- language_detector/models.py
+++ language_detector/models.py
@@ -1,10 +1,25 @@
-from nltk import ngrams
+import nltk
+import numpy as np
+
+from nltk.lm import NgramCounter
+from nltk.lm import Vocabulary
 
 
 class Model(object):
     def __init__(self, name, text):
         self.name = name
         self.text = text
+        self.n = None
+        self.text_grams = None
+        self.dist = None
+        self.vocabs = None
+        self.len = None
+
+    def __str__(self):
+        return "Model(name={}, n={})".format(self.name, self.n)
+
+    def perplexity(self, text):
+        pass
 
     @staticmethod
     def factory(type, **kwargs):
@@ -19,10 +34,50 @@ class Model(object):
 class UnsmoothedModel(Model):
     def __init__(self, name, text):
         super().__init__(name, text)
-        self.n = 3
+        self.n = 2
 
     def train(self):
-        pass
+        self.word_tokens = nltk.tokenize.word_tokenize(self.text)
+        # https://stackoverflow.com/questions/33266956/nltk-package-to-estimate-the-unigram-perplexity/33269399
+        text_grams = nltk.ngrams(
+            self.word_tokens,
+            self.n,
+            pad_right=True,
+            pad_left=True,
+            left_pad_symbol="<s>",
+            right_pad_symbol="</s>",
+        )
+        self.word_tokens.extend(["<s>", "</s>"])
+        self.len = len(self.word_tokens)
+        self.vocabs = Vocabulary(self.word_tokens)
+        self.dist = nltk.FreqDist(text_grams)
+
+    def ngram_probaility(self, text_seq: tuple):
+        log_prob = np.log(
+            self.dist.freq(text_seq) / (self.word_tokens.count(text_seq[0]) / self.len)
+        )
+        # print("{} = {} / ({} / {})".format(log_prob, self.dist.freq(text_seq), self.word_tokens.count(text_seq[0]), self.len))
+        # TODO
+        # - How to handle zeor probaility? for now we return 0
+        return 0.0 if log_prob == float("-inf") else log_prob
+
+    def perplexity(self, text):
+        word_tokens = nltk.word_tokenize(text)
+        text_grams = nltk.ngrams(
+            word_tokens,
+            self.n,
+            pad_right=True,
+            pad_left=True,
+            left_pad_symbol="<s>",
+            right_pad_symbol="</s>",
+        )
+        log_prob = 0
+
+        for token in text_grams:
+            if token[0] in self.vocabs:
+                log_prob += self.ngram_probaility(token)
+
+        return log_prob
 
 
 class LaplaceModel(Model):
@@ -41,3 +96,24 @@ class InterpolationModel(Model):
 
     def train(self):
         pass
+
+
+def test():
+    # text = "the whale is Moby Dick"
+    # test_text = ["Moby Dick", "the whale"]
+    # model = UnsmoothedModel("moby_dick", text)
+    # model.train()
+    # for each in test_text:
+    #     print(model.perplexity(each))
+    with open("data_train/udhr-eng.txt.tra", "r") as train_f, open(
+        "data_dev/udhr-eng.txt.dev", "r"
+    ) as dev_f:
+        data = train_f.read()
+        dev_data = dev_f.read()  # "liberty and the security of person" #dev_f.read()
+        model = UnsmoothedModel("udhr-eng.txt.tra", data)
+        model.train()
+        print(model.perplexity(dev_data))
+
+
+if __name__ == "__main__":
+    test()
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import click
+import csv
 import sys
 import os
+import errno
 
 from language_detector.models import Model
+from typing import List
 
 
 def get_training_files():
     (dirpath, _, filenames) = next(os.walk("data_train"))
+    filenames = sorted(filenames)
     return [
         {"path": os.path.join(dirpath, filename), "name": filename}
         for filename in filenames
@@ -14,28 +18,93 @@ def get_training_files():
     ]
 
 
+def get_dev_files():
+    (dirpath, _, filenames) = next(os.walk("data_dev"))
+    filenames = sorted(filenames)
+    return [
+        {"path": os.path.join(dirpath, filename), "name": filename}
+        for filename in filenames
+        if filename.endswith("txt.dev")
+    ]
+
+
+def train_model(model_type, name, text):
+    model = Model.factory(model_type, name=name, text=text)
+    model.train()
+
+    return model
+
+
+# Taken from https://stackoverflow.com/a/600612/119527
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+# https://stackoverflow.com/a/23794010
+def safe_open_w(path, mode="wt"):
+    mkdir_p(os.path.dirname(path))
+    return open(path, mode)
+
+
+def save_to_tsv(results, filename, output_dir="output"):
+    """
+    Archive language classification output into TSV file
+    """
+    with safe_open_w("{}/{}.tsv".format(output_dir, filename), "wt") as output_file:
+        tsv_writer = csv.writer(output_file, delimiter="\t")
+        rows = [
+            [result["test_name"], result["model_name"], result["perplexity"], result["n"]]
+            for result in results
+        ]
+        tsv_writer.writerows(rows)
+
+
 @click.command(context_settings={"ignore_unknown_options": True})
-@click.argument("model", type=str, required=True, nargs=-1)
+@click.argument("training_model_type", type=str, required=True, nargs=-1)
 @click.option("--debug", type=bool, default=False, help="Enable debug mode.")
-def main(model, debug):
-    """MODEL: --unsmoothed|--laplace|--interpolation"""
-    if model[0] not in ["--unsmoothed", "--laplace", "--interpolation"]:
+def main(training_model_type, debug):
+    """TRAINING_MODEL_TYPE: --unsmoothed|--laplace|--interpolation"""
+    if training_model_type[0] not in ["--unsmoothed", "--laplace", "--interpolation"]:
         print("Unsupported language model.")
         print('Try "main.py --help" for help.')
         sys.exit(1)
 
-    training_model_type = model[0].replace("--", "")
+    training_model_type = training_model_type[0].replace("--", "")
     training_files = get_training_files()
-    models = []
+    models: List(Model) = []
 
     # train model for each language
     for file in training_files:
         with open(file["path"], "r") as input_file:
             data = input_file.read()
-            training_model = Model.factory(training_model_type, name=file["name"], text=data)
-            training_model.train()
+            models.append(train_model(training_model_type, file["name"], data))
 
-            models.append(training_model)
+    dev_files = get_dev_files()
+    dev_results = []
+    for file in dev_files:
+        # compute perplexity for each test file with each language models
+        recorded_perplexity = []
+        with open(file["path"], "r") as input_file:
+            data = input_file.read()
+            for model in models:
+                recorded_perplexity.append(model.perplexity(data))
+        best_model_idx = recorded_perplexity.index(min(recorded_perplexity))
+        best_model = models[best_model_idx]
+        dev_results.append(
+            {
+                "test_name": file["name"],
+                "model_name": best_model.name,
+                "perplexity": recorded_perplexity[best_model_idx],
+                "n": best_model.n,
+            }
+        )
+    save_to_tsv(dev_results, "results_dev_{}".format(training_model_type))
 
 
 if __name__ == "__main__":
