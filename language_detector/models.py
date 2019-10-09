8b0832a05fdfc89f887677e82e774abfac91338f
ybekele <yonael@ualberta.ca>
2019-10-09 17:31:23 -0600

    Fixed interpolation method + made it work with explore.py

--- language_detector/models.py
+++ language_detector/models.py
@@ -136,9 +136,9 @@ class LaplaceModel(Model):
 class InterpolationModel(Model):
     def __init__(self, name, text):
         super().__init__(name, text)
-        self.n = 3
-        self.multi_grams = [None for i in range(self.n)]
-        self.multi_grams_dist = [None for i in range(self.n)]
+        self.n = 1
+        self.multi_grams = [None for i in range(10)]
+        self.multi_grams_dist = [None for i in range(10)]
         self.weights = []
 
     def __str__(self):
@@ -150,7 +150,11 @@ class InterpolationModel(Model):
         tokenizer = CharTokenizer()
         char_tokens = tokenizer.tokenize(self.text)
         self.len = len(char_tokens)
+        #print('this is the len {}'.format(self.len))
+        print("File = {}".format(self.name))
         for n in range(1, self.n + 1):
+            print("this is {}".format(n))
+            print("this is the current len of multi_grams {}".format(len(self.multi_grams)))
             self.multi_grams[n - 1] = nltk.ngrams(char_tokens, n)
             self.multi_grams_dist[n - 1] = nltk.FreqDist(nltk.ngrams(char_tokens, n))
         self.deleted_interpolation()
@@ -164,9 +168,14 @@ class InterpolationModel(Model):
                 cnt = len(gram) - 2  # magic number to help with list slicing for denominator
                 for i in range(1, n + 1):
                     numerator = self.multi_grams_dist[i - 1][gram[-i:]] - 1 # Count(t1, t2, t3) - 1
-                    denominator = (
-                        self.len - 1 if i == 1 else self.multi_grams_dist[i - 2][gram[cnt:-1]] - 1
-                    ) # Count(t1, t2) - 1
+                    if i == 1:
+                        # print('self.len before {}'.format(self.len))
+                        denominator = self.len - 1  
+                        # print('self.len after {}'.format(denominator))
+                        
+                    else:
+                        denominator = self.multi_grams_dist[i - 2][gram[cnt:-1]] - 1
+                
                     logger.debug(
                         "n: {}, gram: {}, numerator: C{}={}, denominator: {}={}".format(
                             i,
@@ -182,7 +191,7 @@ class InterpolationModel(Model):
                     tmp[i - 1] = float(numerator) / float(denominator) if denominator > 0 else 0.0
                 idx = tmp.index(max(tmp)) # get the index of max "value"
                 weights[idx] += self.multi_grams_dist[n - 1][gram] # increment lambda_idx by C(t_i, t_i-n)
-
+        # print("{} {}".format(self.name, weights))
         self.weights = [np.divide(w, np.sum(weights)) for w in weights]
 
     def ngram_probaility(self, text_seq: tuple, n: int, cnt: int):
@@ -212,9 +221,8 @@ class InterpolationModel(Model):
                 prob += self.ngram_probaility(token, n, cnt)
                 if n > 0:
                     cnt -= 1
-            log_prob += prob  # np.log2(prob)
-        return log_prob / self.len
-
+            log_prob += np.log2(prob)
+        return np.power(2, -(1 / len(char_tokens) * log_prob))
 
 def test():
     with open("data_train/udhr-eng.txt.tra", "r") as train_f, open(
